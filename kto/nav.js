d5773c0c9c28c96efc1c735fd79f3b118a923bd6
jjeon02 <june.jeon@gmail.com>
2026-04-24 19:59:19 +0900

    update

--- kto/nav.js
+++ kto/nav.js
@@ -1,12 +1,12 @@
 document.getElementById('nav-top').innerHTML = `
-    <img src="src/0_element/arrow_nav.png" id="icon">
+    <img src="src/assets/arrow_icon.png" id="icon">
     <p class="nav-links">
-        <a href="1_intro.html" >Intro</a>
-        <a href="2_korean.html">Basic Korean</a>
-        <a href="3_essay.html">Essay</a> 
-        <a href="4_sketch.html">Sketches</a>
-        <a href="5_timeline.html">Timeline</a>
-        <a href="6_archive.html">Archive</a>
+        <a href="index.html" >Intro</a>
+        <a href="1_korean.html">Basic Korean</a>
+        <a href="2_essay.html">Essay</a> 
+        <a href="3_sketch.html">Sketches</a>
+        <a href="4_timeline.html">Timeline</a>
+        <a href="5_archive.html">Archive</a>
     </p>
     <p class="nav-menu pointer" onclick="toggleMenu()" >Menu</p>
 `;
